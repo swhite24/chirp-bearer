5a5c90a7e9a16b4c044703282448226847410b92
Steven White <steven.white@ensequence.com>
2014-04-29 11:23:59 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * index.js
+ * Entry point into chirp-bearer
+ *
+ * (C) Steven White 2014
+ */
+
+var request = require('request'),
+	authUrl = 'https://api.twitter.com/oauth2/token',
+	authBody = 'grant_type=client_credentials';
+
+module.exports = function (key, secret) {
+
+	// Encode key / secret
+	var auth = (new Buffer(key + ':' + secret)).toString('base64');
+
+	/**
+	 * get
+	 * - Get new bearer token
+	 */
+	function get (cb) {
+		request({
+			method: 'POST',
+			uri: authUrl,
+			headers: {
+				Authorization: 'Basic ' + auth,
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+			},
+			body: authBody
+		}, function (err, res, body) {
+			cb(err, body);
+		});
+	}
+
+	return {
+		get: get
+	};
+};
